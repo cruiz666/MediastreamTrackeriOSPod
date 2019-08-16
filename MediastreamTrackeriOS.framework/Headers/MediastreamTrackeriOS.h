444bbab7728c3f08ca2b3fff8c0081ce19db8f80
cruiz <ruizcarlos1984@gmail.com>
2019-08-16 18:18:09 -0400

    0.1.0

---
+++ MediastreamTrackeriOS.framework/Headers/MediastreamTrackeriOS.h
@@ -0,0 +1,20 @@
+//
+//  MediastreamTrackeriOS.h
+//  MediastreamTrackeriOS
+//
+//  Created by Carlos Ruiz on 7/12/19.
+//  Copyright © 2019 Carlos Ruiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Version 0.0.1
+
+//! Project version number for MediastreamTrackeriOS.
+FOUNDATION_EXPORT double MediastreamTrackeriOSVersionNumber;
+
+//! Project version string for MediastreamTrackeriOS.
+FOUNDATION_EXPORT const unsigned char MediastreamTrackeriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediastreamTrackeriOS/PublicHeader.h>
+
+
